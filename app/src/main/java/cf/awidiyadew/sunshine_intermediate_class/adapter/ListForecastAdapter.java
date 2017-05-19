0fbaf4c81b1fabd3ac79482c5f070404f385ba7a
awidiyadew <awidiya.dewa@gmail.com>
2017-05-19 10:41:05 +0800

    [run app] merubah adapter untuk menyesuaikan dg view holder

--- app/src/main/java/cf/awidiyadew/sunshine_intermediate_class/adapter/ListForecastAdapter.java
+++ app/src/main/java/cf/awidiyadew/sunshine_intermediate_class/adapter/ListForecastAdapter.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cf.awidiyadew.sunshine_intermediate_class.R;
-import cf.awidiyadew.sunshine_intermediate_class.model.DummyForecast;
 import cf.awidiyadew.sunshine_intermediate_class.model.ListForecast;
 
 /**
@@ -19,6 +18,8 @@ import cf.awidiyadew.sunshine_intermediate_class.model.ListForecast;
 public class ListForecastAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<ListForecast> listData = new ArrayList<>();
+    private static final int TODAY_VIEW = 0;
+    private static final int NEXT_DAY_VIEW = 1;
 
     public ListForecastAdapter(List<ListForecast> listData) {
         this.listData = listData;
@@ -26,18 +27,35 @@ public class ListForecastAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.row_forecast_item, parent, false);
-
-        return new ForecastItemViewHolder(itemView);
+        // TODO: 5/19/17 step 2
+        // Jika view typenya adalah TODAY_VIEW maka return ViewHolder TodayForecastItemViewHolder dengan layout row_today...
+        if (viewType == TODAY_VIEW){
+            View todayItemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.row_today_forecast_item, parent, false);
+            return new TodayForecastItemViewHolder(todayItemView);
+        } else {
+            // Jika view typenya bukan TODAY_VIEW maka return ViewHolder biasa yaitu ForecastItemViewHolder dengan layout row_forecast_item...
+            View nextDayItemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.row_forecast_item, parent, false);
+            return new ForecastItemViewHolder(nextDayItemView);
+        }
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        ForecastItemViewHolder forecastItemViewHolder = (ForecastItemViewHolder) holder;
-        ListForecast data = listData.get(position);
-        forecastItemViewHolder.bind(data, position);
+        // TODO: 5/19/17 step 3 : bind data
+
+        final int viewType = getItemViewType(position);
+        final ListForecast data = listData.get(position);
+
+        if (viewType == TODAY_VIEW){
+            TodayForecastItemViewHolder todayForecastItemViewHolder = (TodayForecastItemViewHolder) holder; // casting view holder menjadi TodayForecastItemViewHolder
+            todayForecastItemViewHolder.bind(data, position);
+        } else {
+            ForecastItemViewHolder forecastItemViewHolder = (ForecastItemViewHolder) holder; // casting view holder menjadi ForecastItemViewHolder
+            forecastItemViewHolder.bind(data, position);
+        }
+
     }
 
     @Override
@@ -45,4 +63,13 @@ public class ListForecastAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         return listData.size();
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        // TODO: 5/19/17 step 1
+        // posisi 0 (item pertama), jika posisi nol maka set view type menjadi TODAY_VIEW
+        if (position == 0)
+            return TODAY_VIEW;
+        else
+            return NEXT_DAY_VIEW;
+    }
 }
