538fd4aa2abf5f9235f1e1d6647d94bdb3161852
awidiyadew <awidiya.dewa@gmail.com>
2017-05-20 11:55:10 +0800

    menambahkan class Application Sunshine dan set ke manifest

---
+++ app/src/main/java/cf/awidiyadew/sunshine_intermediate_class/Sunshine.java
@@ -0,0 +1,35 @@
+package cf.awidiyadew.sunshine_intermediate_class;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Created by awidiyadew on 5/20/17.
+ */
+
+public class Sunshine extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Create an InitializerBuilder
+        Stetho.InitializerBuilder initializerBuilder =
+                Stetho.newInitializerBuilder(this);
+
+        // Enable Chrome DevTools
+        initializerBuilder.enableWebKitInspector(
+                Stetho.defaultInspectorModulesProvider(this)
+        );
+
+        // Enable command line interface
+        initializerBuilder.enableDumpapp(
+                Stetho.defaultDumperPluginsProvider(this)
+        );
+
+        // Use the InitializerBuilder to generate an Initializer
+        Stetho.Initializer initializer = initializerBuilder.build();
+
+        // Initialize Stetho with the Initializer
+        Stetho.initialize(initializer);
+    }
+}
